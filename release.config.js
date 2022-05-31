2411dd1224a9eef7a19bc1d4b55c6fd060c0792f
lpm0073 <lpm0073@gmail.com>
2022-05-31 12:04:03 -0500

    resolve merge conflict

--- release.config.js
+++ release.config.js
@@ -14,7 +14,7 @@ module.exports = {
         "@semantic-release/git",
         {
           "assets": [
-            "CHANGELOG.md"
+            "CHANGELOG.md",
           ],
           "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }
