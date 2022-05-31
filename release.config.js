010da3364919419e3ee362214b9e809058d69f6d
lpm0073 <lpm0073@gmail.com>
2022-05-31 11:58:54 -0500

    add PAT for post

--- release.config.js
+++ release.config.js
@@ -6,7 +6,7 @@ module.exports = {
       [
         "@semantic-release/changelog",
         {
-          "changelogFile": "docs/CHANGELOG.md"
+          "changelogFile": "CHANGELOG.md"
         }
       ],
       "@semantic-release/github",
@@ -14,7 +14,7 @@ module.exports = {
         "@semantic-release/git",
         {
           "assets": [
-            "docs/CHANGELOG.md",
+            "CHANGELOG.md",
             "package.json",
             "package-lock.json"
           ],
